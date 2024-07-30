bbc3d8462ee2d377427c8484031c82c2eabeef62
EricBrown56 <groove505@gmail.com>
2024-07-29 23:45:43 -0500

    first commit

---
+++ jsHomework.js
@@ -0,0 +1,36 @@
+// Assignment 1
+
+const studentName = 'John Doe';
+console.log(studentName);
+
+const studentAge = 16;
+console.log(studentAge);
+
+const studentGrade = 98;
+console.log(studentGrade);
+
+const studentAttendance = 0.9;
+console.log(studentAttendance);
+
+// Assignment 2
+
+const a = 5;
+const b = 10;
+
+console.log("Sum:", a + b);
+console.log("Difference:", a - b);
+console.log("Product:", a * b);
+console.log("Quotient:", a / b);
+
+console.log("Updated a:", a + 1);
+console.log("Updated b:", b - 1);
+
+console.log("is a equal to b?", a == b);
+console.log("is a greater than b?", a > b);
+console.log("is a not equal to b?", a != b);
+
+let isPositive = a > 0 && b > 0;
+console.log("Are both a and b positive?", isPositive);
+
+let isEven = a % 2 == 0 || b % 2 == 0;
+console.log("Is either a or b even?", isEven);
